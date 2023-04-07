066b1dc6ffb7ed74426786e90f776ad39b9cf31e
Ivan Yaremko <yaremich98@gmail.com>
2023-04-07 06:55:05 +0300

    (106) React. Redux. Saga

---
+++ Redux/src/components/UsersList.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import * as UserActionCreators from '../actions/userCreators';
+
+const UsersList = (props) => {
+    const {users, isFetching, error, getUsersReq} = props;
+    useEffect(() => { getUsersReq() }, [getUsersReq]);
+    return (
+        <section>
+            <h2>Users List</h2>
+            {isFetching && 'Loading...'}
+            {error && JSON.stringify(error)}
+            <ul>
+                {users.map(u =>(<li key = {u.id}>{u.email}</li>))} 
+            </ul>
+            <button onClick={() => {getUsersReq({offset:users.length})}}>load more</button>
+        </section>
+    );
+}
+
+const mapStateToProps = ({users}) => users;
+const mapDispatchToProps = (dispatch) => ({
+    getUsersReq:({limit, offset}={}) => dispatch(UserActionCreators.getUsersRequest({limit, offset}))
+});
+export default connect(mapStateToProps)(UsersList);
